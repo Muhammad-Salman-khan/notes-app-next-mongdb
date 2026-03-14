a4f37a3cc72d259f7cc515fea2093ccb8e9f17c1
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-03-14 12:04:15 +0500

    fix: remove unused Toast import from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Toast from "@/utils/Toast";
 import ThemeProvider from "@/context/ThemeContext";
 import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
