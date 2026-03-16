f93ab00c545023e36d6e5e8faa19830de10a4672
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-03-16 15:59:04 +0500

    style: improve layout and spacing in save-notes page for better readability

--- src/app/save-notes/[id]/page.tsx
+++ src/app/save-notes/[id]/page.tsx
@@ -26,19 +26,21 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   return (
     <>
-      <div className="max-w-2xl mx-auto px-4 py-8">
+      <div className="max-w-2xl mx-auto px-3 sm:px-4 py-6 sm:py-8">
         <Link href={`/save-notes/`}>
-          <Button className="text-sm  mb-8 font-extrabold flex items-center gap-1  transition-colors">
+          <Button className="text-sm mb-6 sm:mb-8 font-extrabold flex items-center gap-1 transition-colors">
             <StepBack /> back to notes
           </Button>
         </Link>
 
-        <div className="border rounded-xl p-8 bg-background">
-          <div className="flex items-start justify-between gap-4 mb-6">
-            <h1 className="text-2xl font-medium">{data.title}</h1>
+        <div className="border rounded-xl p-4 sm:p-6 md:p-8 bg-background">
+          <div className="flex flex-col sm:flex-row sm:items-start justify-between gap-3 sm:gap-4 mb-6">
+            <h1 className="text-xl sm:text-2xl font-medium warp-break-words">
+              {data.title}
+            </h1>
             <div className="flex gap-2 shrink-0">
               <Link href={`/save-notes/${id}/edit`}>
-                <Button className="text-xs px-3 font-extrabold py-1.5 rounded-md border ">
+                <Button className="text-xs px-3 font-extrabold py-1.5 rounded-md border">
                   Edit
                 </Button>
               </Link>
@@ -47,17 +49,17 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
           </div>
 
           <div className="border-t pt-6 mb-6">
-            <p className="text-sm leading-relaxed whitespace-pre-wrap">
+            <p className="text-sm leading-relaxed whitespace-pre-wrap wrap-break-word">
               {data.content}
             </p>
           </div>
 
-          <div className="border-t pt-4 flex justify-between">
+          <div className="border-t pt-4 flex flex-col sm:flex-row sm:justify-between gap-3 sm:gap-0">
             <div>
               <p className="text-xs text-muted-foreground mb-1">created</p>
               <p className="text-sm">{formattedDate(data.createdAt)}</p>
             </div>
-            <div className="text-right">
+            <div className="sm:text-right">
               <p className="text-xs text-muted-foreground mb-1">last updated</p>
               <p className="text-sm">{formattedDate(data.updatedAt)}</p>
             </div>
