adf213cae71c7aa17aeef3663adce3ee177c4652
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-03-21 19:45:32 +0500

    feat: Add pages for creating and viewing notes.

--- src/app/save-notes/page.tsx
+++ src/app/save-notes/page.tsx
@@ -1,50 +1,54 @@
-export const dynamic = "force-dynamic";
 import NotesCard from "@/components/meteors-demo";
 import { getNewData } from "@/server/action";
 import { dataType } from "../page";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { connection } from "next/server";
 const page = async () => {
- let data: any;
- try {
-  const result = await getNewData();
-  if (!result.success || !result.data) {
-   setTimeout(() => notFound(), 500);
+  await connection();
+  let data: any;
+  try {
+    const result = await getNewData();
+    if (!result.success || !result.data) {
+      notFound();
+    }
+    data = result.data;
+  } catch (error: any) {
+    console.error(`failed to fetch data, ${error}`);
   }
-  data = result.data;
- } catch (error: any) {
-  console.error(`failed to fetch data, ${error}`);
- }
- return (
-  <>
-   <div className="min-h-screen">
-    <div className="w-full flex flex-col p-4  text-center gap-2 md:flex-row md:items-center md:justify-between md:gap-6">
-     <h2 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-foreground">
-      See your All Notes
-     </h2>
-     <p className="text-sm sm:text-base font-extrabold whitespace-nowrap cursor-pointer shrink-0">
-      <Link href={"/"}>Create Notes!</Link>
-     </p>
-    </div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-4 py-6 max-w-8xl mx-auto">
-     {data.length > 0 ? (
-      data?.map(({ _id, title, content, createdAt, updatedAt }: dataType) => (
-       <NotesCard
-        id={_id?.toString()}
-        key={_id?.toString()}
-        title={title}
-        content={content}
-        createdAt={createdAt}
-        updatedAt={updatedAt}
-       />
-      ))
-     ) : (
-      <p className="text-center font-extrabold text-2xl">No Notes available!</p>
-     )}
-    </div>
-   </div>
-  </>
- );
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="w-full flex flex-col p-4  text-center gap-2 md:flex-row md:items-center md:justify-between md:gap-6">
+          <h2 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-foreground">
+            See your All Notes
+          </h2>
+          <p className="text-sm sm:text-base font-extrabold whitespace-nowrap cursor-pointer shrink-0">
+            <Link href={"/"}>Create Notes!</Link>
+          </p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-4 py-6 max-w-8xl mx-auto">
+          {data.length > 0 ?
+            data?.map(
+              ({ _id, title, content, createdAt, updatedAt }: dataType) => (
+                <NotesCard
+                  id={_id?.toString()}
+                  key={_id?.toString()}
+                  title={title}
+                  content={content}
+                  createdAt={createdAt}
+                  updatedAt={updatedAt}
+                />
+              ),
+            )
+          : <p className="text-center font-extrabold text-2xl">
+              No Notes available!
+            </p>
+          }
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default page;
--- src/components/client_components/Full.tsx
+++ src/components/client_components/Full.tsx
@@ -6,62 +6,68 @@ import { Button } from "../ui/button";
 import { toast } from "sonner";
 import { PostNote } from "@/server/action";
 export type createPostType = {
- title: string;
- content: string;
+  title: string;
+  content: string;
 };
 export const placeholders = [
- "Write a quick note before you forget...",
- "What's on your mind right now?",
+  "Write a quick note before you forget...",
+  "What's on your mind right now?",
 ];
 const SaveNotes = ({ Heading }: { Heading: string }) => {
- const [title, setTitle] = useState<string>("");
- const [content, setContent] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
- const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  try {
-   e.preventDefault();
-   if (!title || !content) {
-    return toast.error("Fill all the fields");
-   }
-   const newPost: createPostType = {
-    title,
-    content,
-   };
-   const result = await PostNote(newPost);
-   setContent("");
-   if (!result?.success) {
-    return toast.error(`failed to Post`);
-   }
-   toast.success(`Post created successfully ${result.data.title}`);
-  } catch (error: any) {
-   return toast.error(error);
-  }
- };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      if (!title || !content) {
+        return toast.error("Fill all the fields");
+      }
+      const newPost: createPostType = {
+        title,
+        content,
+      };
+      const result = await PostNote(newPost);
+      setLoading(!loading);
+      setContent("");
+      if (!result?.success) {
+        return toast.error(`failed to Post`);
+      }
+      toast.success(`Post created successfully ${result.data.title}`);
+    } catch (error: any) {
+      return toast.error(error);
+    }
+  };
 
- return (
-  <div className="max-h-3/12 w-full flex flex-col items-center justify-center px-4 py-12">
-   <div className="w-full max-w-2xl flex flex-col gap-6">
-    <h2 className="font-extrabold text-2xl sm:text-4xl md:text-5xl text-center text-foreground">
-     {Heading}
-    </h2>
-    <PlaceholdersAndVanishInput
-     placeholders={placeholders}
-     onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-      setTitle(e.target.value)
-     }
-    />
-    <Textarea
-     value={content}
-     onChange={(e) => setContent(e.target.value)}
-     placeholder="Write your note here..."
-     className="min-h-40 sm:min-h-56 resize-none"
-    />
-    <Button disabled={!content} type="submit" onClick={(e: any) => onSubmit(e)}>
-     Save Note
-    </Button>
-   </div>
-  </div>
- );
+  return (
+    <div className="max-h-3/12 w-full flex flex-col items-center justify-center px-4 py-12">
+      <div className="w-full max-w-2xl flex flex-col gap-6">
+        <h2 className="font-extrabold text-2xl sm:text-4xl md:text-5xl text-center text-foreground">
+          {Heading}
+        </h2>
+        <PlaceholdersAndVanishInput
+          placeholders={placeholders}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+        />
+        <Textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Write your note here..."
+          className="min-h-40 sm:min-h-56 resize-none"
+        />
+        <Button
+          disabled={!content || !title || loading}
+          type="submit"
+          onClick={(e: any) => onSubmit(e)}
+        >
+          Save Note
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default SaveNotes;
